2cc20fe28222c6823ac19b0f1b734d7cfd2fea91
Dmitrii <dtihonov982@gmail.com>
2023-08-03 10:09:48 +0300

    Connect logger.cpp to AccessLogger

--- src/logger.cpp
+++ src/logger.cpp
@@ -6,16 +6,46 @@
 #include <fstream>
 #include <random>
 
-#include "Logger.h"
+#include "AccessLogger.h"
+#include "NotifyingIterator.h"
 #include "Common.h"
 
+void sort() {
+    std::vector<int> data(10);
+    std::iota(data.rbegin(), data.rend(), 0);
 
+    std::ofstream file{"logs/sort.txt"};
+    file << "title\n" << data << '\n';
 
+    AccessLogger logger{data, file};
 
+    auto begin_ = NotifyingIterator(data.begin(), logger);
+    auto end_ = NotifyingIterator(data.end(), logger);
+
+    std::sort(begin_, end_);
+
+    logger.finalize();
+}
+
+void partial_sum() {
+    std::vector<int> data = getRandVector(50, 0, 100);
+    std::ofstream file{"logs/partial_sum.txt"};
+    file << "title\n" << data << '\n';
+
+    AccessLogger logger{data, file};
+    auto begin_ = NotifyingIterator(data.begin(), logger);
+    auto end_ = NotifyingIterator(data.end(), logger);
+
+    std::partial_sum(begin_, end_, begin_);
+
+    logger.finalize();
+}
 
 int main() {
-    std::random_device rd;
-    std::mt19937 g(rd());
+    sort();
+    partial_sum();
+    //std::random_device rd;
+    //std::mt19937 g(rd());
     //std::vector<int> data {10, 20};
     //std::vector<int> data(100);
     //std::iota(data.rbegin(), data.rend(), 0);
@@ -26,14 +56,6 @@ int main() {
     
     */
 
-    std::vector<int> data = getRandVector(50, 0, 100);
-    std::ofstream file{"partial_sum.txt"};
-    file << "title\n" << data << '\n';
-
-    AccessLogger logger{data, file};
-    auto begin_ = IterDecorator(data.begin(), logger);
-    auto middle_ = IterDecorator(data.begin() + 50, logger);
-    auto end_ = IterDecorator(data.end(), logger);
 
     //std::sort(begin_, end_);
     //std::rotate(begin_, middle_, end_);
@@ -49,9 +71,6 @@ int main() {
     //assert(std::is_sorted(begin_, end_));
     //assert(*std::max_element(begin_, end_) == 9);
     //std::binary_search(begin_, end_, 1);
-    std::partial_sum(begin_, end_, begin_);
-
-    logger.finalize();
 
 
     return 0;
