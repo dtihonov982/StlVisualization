3ebb5f977881f36581fd69ebd71b4e54cb207c01
Dmitrii <dtihonov982@gmail.com>
2023-08-12 11:18:05 +0300

    Resolve confilcts

--- src/Common/Script.h
+++ src/Common/Script.h
@@ -1,17 +1,23 @@
-#ifndef JOURNAL_H
-#define JOURNAL_H
+#ifndef SCRIPT_H
+#define SCRIPT_H
 
 #include <iostream>
 #include <string>
 #include <vector>
 
-using Entry = std::vector<std::string>;
-using Journal = std::vector<Entry>;
+struct Action {
+    enum Type { ACCESS, WRITE };
+    Type type;
+    size_t pos;
+    int value;
+};
+
+using Script = std::vector<Action>;
 
 std::vector<std::string> split(const std::string& input, char delim);
 
-Journal readJournal(std::istream& is, char delim);
+std::vector<Action> readScript(std::istream& is, char delim);
 
 std::vector<int> loadDataFromDump(const std::string& dump, char delim);
 
-#endif //JOURNAL_H
+#endif //SCRIPT_H
